f14e938d483a5cf36945a9ee13652479f94f8355
Bustiss <williejones321@gmail.com>
2024-09-04 16:27:24 -0500

    Initial Commit

---
+++ ChefCademy/ChefCademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChefCademyApp.swift
+//  ChefCademy
+//
+//  Created by Willie Jones on 9/4/24.
+//
+
+import SwiftUI
+
+@main
+struct ChefCademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChefCademy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChefCademy
+//
+//  Created by Willie Jones on 9/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
